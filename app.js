4e3c7ed4a448fda5e9cd05e074b8af286bc13893
shahjee362 <ahmedshah222666@gmail.com>
2023-10-01 16:12:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain,numbers, $ and_. For example : $my_1stVariable</p>");
+document.write("<p>Variables must begin with a,letter, $ or _. For example :  $name, _name or name</p>");
+document.write("<p>Variable names are case Sensitive</p>");
+document.write("<p>Variable names should not be JS Keywords</p>");
